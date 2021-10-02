e5307bdc80fbf95a9e8446db29593e2d777e599b
cxblovecw <873587091@qq.com>
2021-10-03 03:18:33 +0800

    fix: 设置base

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: process.env.NODE_ENV = 'production'
-  ? '/StudyTravis/'
-  : '/',
-  build: {
-    assetsDir: process.env.NODE_ENV = 'production'
-    ? '/StudyTravis/assets/'
-    : '/',
-  }
+  base: process.env.NODE_ENV = 'production' ? '/StudyTravis/' : '/'
 })
