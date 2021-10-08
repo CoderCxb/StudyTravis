22455a5dbef4dd7c44331e5df8cbe0053d1a270a
陈小斌 <chenxiaobin@chenxiaobindeMacBook-Pro.local>
2021-10-08 18:58:02 +0800

    fix: 添加git操作

---
+++ scripts/update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+echo "update脚本开始执行";
+
+
+if [ ! -d "GitStudy" ]
+then
+  # cd ../.. 克隆文件的位置，之后要修改到项目外
+  git clone https://github.com/cxblovecw/GitStudy.git
+  echo '开始克隆'
+fi
+
+cd GitStudy
+# 切换分支
+git checkout -B feature/update-vis-ui-$(date "+%Y%m%d")
+git push --set-upstream origin feature/update-vis-ui-$(date "+%Y%m%d")
+echo "仓库存在，进入仓库"
+echo "进行更新"
+git checkout master
+cd ..
+echo "退出仓库"
+
+# `echo "gh -v"`
+
+echo "update脚本执行完毕";
